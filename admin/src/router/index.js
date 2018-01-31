27a0060e317016de7b19d369638223ce7dbe4192
shusong <shusong@qq.com>
2018-01-31 17:32:33 +0800

    提交修改

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '../pages/login'
+import Home from '../pages/home'
 
 Vue.use(Router)
 
@@ -8,8 +9,20 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect:'/login'
+    },
+    {
+      path: '/login',
+      name:'Login',
+      component:Login
+    },
+    {
+      path: '/index',
+      name:'Home',
+      component:Home,
+      meta:{
+        requireAuth: true
+      }
     }
   ]
 })
